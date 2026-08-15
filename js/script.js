e2fd5a69063bbd62546b97cf07264ba6e3411d64
Hafizanisha <hafizanisha81@gmail.com>
2026-08-16 00:32:56 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+// Smooth scrolling for navigation links
 
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
